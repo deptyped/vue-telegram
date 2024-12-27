ad538e58459f7c6b19b4d7c8c4e561ddd7ecd6ff
deptyped <deptyped@gmail.com>
2024-12-27 21:19:25 +0200

    Implement useBackButton

--- src/composables-legacy/useWebAppBackButton.ts
+++ src/composables-legacy/useWebAppBackButton.ts
@@ -1,49 +1,16 @@
-import { computed, ref } from 'vue'
+import { useBackButton } from '../composables/useBackButton'
 import { onBackButtonClicked } from '../events'
-import { WebApp } from '../sdk'
-import { defineStore } from '../utils'
-
-const useStore = defineStore(() => {
-  const isBackButtonVisible = ref(WebApp.BackButton.isVisible)
-
-  function updateState() {
-    isBackButtonVisible.value = WebApp.BackButton.isVisible
-  }
-
-  function showBackButton(
-    ...params: Parameters<typeof WebApp.BackButton.show>
-  ) {
-    WebApp.BackButton.show(...params)
-    updateState()
-  }
-
-  function hideBackButton(
-    ...params: Parameters<typeof WebApp.BackButton.hide>
-  ) {
-    WebApp.BackButton.hide(...params)
-    updateState()
-  }
-
-  return { isBackButtonVisible, showBackButton, hideBackButton }
-})
 
 /**
  * @deprecated Use [`useBackButton`](https://vue-tg.deptyped.com/mini-apps.html#usebackbutton) instead
  */
 export function useWebAppBackButton() {
-  const { isBackButtonVisible, showBackButton, hideBackButton } = useStore()
+  const backButton = useBackButton({ version: '8.0' })
 
   return {
-    isBackButtonVisible: computed({
-      get() {
-        return isBackButtonVisible.value
-      },
-      set(isVisible) {
-        isVisible ? showBackButton() : hideBackButton()
-      },
-    }),
-    showBackButton,
-    hideBackButton,
+    isBackButtonVisible: backButton.isVisible,
+    showBackButton: backButton.show,
+    hideBackButton: backButton.hide,
     /**
      * @deprecated import directly from `vue-tg` instead.
      */
