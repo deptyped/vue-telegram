1c4cb8097acd39fbe14074c44c7555f9e2fd7b08
deptyped <deptyped@gmail.com>
2024-12-27 21:22:32 +0200

    Implement useTheme

--- src/composables-legacy/useWebAppTheme.ts
+++ src/composables-legacy/useWebAppTheme.ts
@@ -1,47 +1,6 @@
-import { computed, readonly, ref } from 'vue'
+import type { WebApp } from '../sdk'
+import { useTheme } from '../composables/useTheme'
 import { onThemeChanged } from '../events'
-import { WebApp } from '../sdk'
-import { defineStore } from '../utils'
-
-const useStore = defineStore(() => {
-  const colorScheme = ref(WebApp.colorScheme)
-  const themeParams = ref(WebApp.themeParams)
-  const headerColor = ref(WebApp.headerColor)
-  const backgroundColor = ref(WebApp.backgroundColor)
-
-  function updateState() {
-    colorScheme.value = WebApp.colorScheme
-    themeParams.value = {
-      ...WebApp.themeParams,
-    }
-    headerColor.value = WebApp.headerColor
-    backgroundColor.value = WebApp.backgroundColor
-  }
-
-  function setHeaderColor(
-    ...params: Parameters<typeof WebApp.setHeaderColor>
-  ) {
-    WebApp.setHeaderColor(...params)
-    updateState()
-  }
-
-  function setBackgroundColor(
-    ...params: Parameters<typeof WebApp.setBackgroundColor>
-  ) {
-    WebApp.setBackgroundColor(...params)
-    updateState()
-  }
-
-  return {
-    colorScheme,
-    themeParams,
-    headerColor,
-    backgroundColor,
-    updateState,
-    setHeaderColor,
-    setBackgroundColor,
-  }
-})
 
 /**
  * @deprecated Use [`useTheme`](https://vue-tg.deptyped.com/mini-apps.html#usetheme) instead
@@ -52,34 +11,19 @@ export function useWebAppTheme() {
     themeParams,
     headerColor,
     backgroundColor,
-    updateState,
-    setHeaderColor,
-    setBackgroundColor,
-  } = useStore()
-
-  onThemeChanged(updateState)
+  } = useTheme({ version: '8.0' })
 
   return {
-    colorScheme: readonly(colorScheme),
-    themeParams: readonly(themeParams),
-    headerColor: computed({
-      get() {
-        return headerColor.value
-      },
-      set(newValue) {
-        setHeaderColor(newValue)
-      },
-    }),
-    backgroundColor: computed({
-      get() {
-        return backgroundColor.value
-      },
-      set(newValue) {
-        setBackgroundColor(newValue)
-      },
-    }),
-    setHeaderColor,
-    setBackgroundColor,
+    colorScheme,
+    themeParams,
+    headerColor,
+    backgroundColor,
+    setHeaderColor(color: Parameters<WebApp['setHeaderColor']>[0]) {
+      headerColor.value = color
+    },
+    setBackgroundColor(color: Parameters<WebApp['setBackgroundColor']>[0]) {
+      backgroundColor.value = color
+    },
     /**
      * @deprecated import directly from `vue-tg` instead.
      */
