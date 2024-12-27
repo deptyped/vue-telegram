42367316b36f23a8ec1f1e0db9f69a3831e310b3
deptyped <deptyped@gmail.com>
2024-12-27 21:21:33 +0200

    Implement useMainButton

---
+++ docs/mini-apps/composables/use-main-button.md
@@ -0,0 +1,27 @@
+### useMainButton
+
+```ts
+import { useMainButton } from 'vue-tg'
+
+const mainButton = useMainButton()
+```
+ 
+| Name                | Description                                                                                               |
+| :------------------ | :-------------------------------------------------------------------------------------------------------- |
+| `text`              | <!--@include: @/generated/MainButton-text.md --><br/><Badge type="info" text="⚡️ reactive" />              |
+| `color`             | <!--@include: @/generated/MainButton-color.md --><br/><Badge type="info" text="⚡️ reactive" />             |
+| `textColor`         | <!--@include: @/generated/MainButton-textColor.md --><br/><Badge type="info" text="⚡️ reactive" />         |
+| `isVisible`         | <!--@include: @/generated/MainButton-isVisible.md --><br/><Badge type="info" text="⚡️ reactive" />         |
+| `isActive`          | <!--@include: @/generated/MainButton-isActive.md --><br/><Badge type="info" text="⚡️ reactive" />          |
+| `isProgressVisible` | <!--@include: @/generated/MainButton-isProgressVisible.md --><br/><Badge type="info" text="⚡️ reactive" /> |
+| `hasShineEffect`    | <!--@include: @/generated/MainButton-hasShineEffect.md --><br/><Badge type="info" text="⚡️ reactive" />    |
+| `show`              | <!--@include: @/generated/MainButton-show.md -->                                                          |
+| `hide`              | <!--@include: @/generated/MainButton-hide.md -->                                                          |
+| `enable`            | <!--@include: @/generated/MainButton-enable.md -->                                                        |
+| `disable`           | <!--@include: @/generated/MainButton-disable.md -->                                                       |
+| `showProgress`      | <!--@include: @/generated/MainButton-showProgress.md -->                                                  |
+| `hideProgress`      | <!--@include: @/generated/MainButton-hideProgress.md -->                                                  |
+| `setParams`         | <!--@include: @/generated/MainButton-setParams.md -->                                                     |
+| `onClick`           | <!--@include: @/generated/MainButton-onClick.md -->                                                       |
+| `version`           | <!--@include: @/generated/WebApp-version.md -->                                                           |
+| `isVersionAtLeast`  | <!--@include: @/generated/WebApp-isVersionAtLeast.md -->                                                  |
--- src/composables-legacy/useWebAppMainButton.ts
+++ src/composables-legacy/useWebAppMainButton.ts
@@ -1,184 +1,30 @@
-import { computed, ref } from 'vue'
+import type { WebApp } from '../sdk'
+import { useMainButton } from '../composables/useMainButton'
 import { onMainButtonClicked } from '../events'
-import { WebApp } from '../sdk'
-import { defineStore } from '../utils'
-
-const useStore = defineStore(() => {
-  const mainButtonText = ref(WebApp.MainButton.text)
-  const mainButtonColor = ref(WebApp.MainButton.color)
-  const mainButtonTextColor = ref(WebApp.MainButton.textColor)
-  const isMainButtonVisible = ref(WebApp.MainButton.isVisible)
-  const isMainButtonActive = ref(WebApp.MainButton.isActive)
-  const isMainButtonProgressVisible = ref(
-    WebApp.MainButton.isProgressVisible,
-  )
-
-  function updateState() {
-    mainButtonText.value = WebApp.MainButton.text
-    mainButtonColor.value = WebApp.MainButton.color
-    mainButtonTextColor.value = WebApp.MainButton.textColor
-    isMainButtonVisible.value = WebApp.MainButton.isVisible
-    isMainButtonActive.value = WebApp.MainButton.isActive
-    isMainButtonProgressVisible.value
-      = WebApp.MainButton.isProgressVisible
-  }
-
-  function setMainButtonText(
-    ...params: Parameters<typeof WebApp.MainButton.setText>
-  ) {
-    WebApp.MainButton.setText(...params)
-    updateState()
-  }
-
-  function showMainButton(
-    ...params: Parameters<typeof WebApp.MainButton.show>
-  ) {
-    WebApp.MainButton.show(...params)
-    updateState()
-  }
-
-  function hideMainButton(
-    ...params: Parameters<typeof WebApp.MainButton.hide>
-  ) {
-    WebApp.MainButton.hide(...params)
-    updateState()
-  }
-
-  function enableMainButton(
-    ...params: Parameters<typeof WebApp.MainButton.enable>
-  ) {
-    WebApp.MainButton.enable(...params)
-    updateState()
-  }
-
-  function disableMainButton(
-    ...params: Parameters<typeof WebApp.MainButton.disable>
-  ) {
-    WebApp.MainButton.disable(...params)
-    updateState()
-  }
-
-  function showMainButtonProgress(
-    ...params: Parameters<typeof WebApp.MainButton.showProgress>
-  ) {
-    WebApp.MainButton.showProgress(...params)
-    updateState()
-  }
-
-  function hideMainButtonProgress(
-    ...params: Parameters<typeof WebApp.MainButton.hideProgress>
-  ) {
-    WebApp.MainButton.hideProgress(...params)
-    updateState()
-  }
-
-  function setMainButtonParams(
-    ...params: Parameters<typeof WebApp.MainButton.setParams>
-  ) {
-    WebApp.MainButton.setParams(...params)
-    updateState()
-  }
-
-  return {
-    mainButtonText,
-    mainButtonColor,
-    mainButtonTextColor,
-    isMainButtonVisible,
-    isMainButtonActive,
-    isMainButtonProgressVisible,
-    setMainButtonText,
-    showMainButton,
-    hideMainButton,
-    enableMainButton,
-    disableMainButton,
-    showMainButtonProgress,
-    hideMainButtonProgress,
-    setMainButtonParams,
-  }
-})
 
 /**
  * @deprecated Use [`useMainButton`](https://vue-tg.deptyped.com/mini-apps.html#usemainbutton) instead
  */
 export function useWebAppMainButton() {
-  const {
-    mainButtonText,
-    mainButtonColor,
-    mainButtonTextColor,
-    isMainButtonVisible,
-    isMainButtonActive,
-    isMainButtonProgressVisible,
-    setMainButtonText,
-    showMainButton,
-    hideMainButton,
-    enableMainButton,
-    disableMainButton,
-    showMainButtonProgress,
-    hideMainButtonProgress,
-    setMainButtonParams,
-  } = useStore()
+  const mainButton = useMainButton({ version: '8.0' })
 
   return {
-    mainButtonText: computed({
-      get() {
-        return mainButtonText.value
-      },
-      set(text) {
-        setMainButtonText(text)
-      },
-    }),
-    mainButtonColor: computed({
-      get() {
-        return mainButtonColor.value
-      },
-      set(color) {
-        setMainButtonParams({
-          color,
-        })
-      },
-    }),
-    mainButtonTextColor: computed({
-      get() {
-        return mainButtonTextColor.value
-      },
-      set(color) {
-        setMainButtonParams({
-          text_color: color,
-        })
-      },
-    }),
-    isMainButtonVisible: computed({
-      get() {
-        return isMainButtonVisible.value
-      },
-      set(isVisible) {
-        isVisible ? showMainButton() : hideMainButton()
-      },
-    }),
-    isMainButtonActive: computed({
-      get() {
-        return isMainButtonActive.value
-      },
-      set(isActive) {
-        isActive ? enableMainButton() : disableMainButton()
-      },
-    }),
-    isMainButtonProgressVisible: computed({
-      get() {
-        return isMainButtonProgressVisible.value
-      },
-      set(isProgressVisible) {
-        isProgressVisible ? showMainButtonProgress() : hideMainButtonProgress()
-      },
-    }),
-    setMainButtonText,
-    showMainButton,
-    hideMainButton,
-    enableMainButton,
-    disableMainButton,
-    showMainButtonProgress,
-    hideMainButtonProgress,
-    setMainButtonParams,
+    mainButtonText: mainButton.text,
+    mainButtonColor: mainButton.color,
+    mainButtonTextColor: mainButton.textColor,
+    isMainButtonVisible: mainButton.isVisible,
+    isMainButtonActive: mainButton.isActive,
+    isMainButtonProgressVisible: mainButton.isProgressVisible,
+    setMainButtonText(text: Parameters<WebApp['MainButton']['setText']>[0]) {
+      mainButton.text.value = text
+    },
+    showMainButton: mainButton.show,
+    hideMainButton: mainButton.hide,
+    enableMainButton: mainButton.enable,
+    disableMainButton: mainButton.disable,
+    showMainButtonProgress: mainButton.showProgress,
+    hideMainButtonProgress: mainButton.hideProgress,
+    setMainButtonParams: mainButton.setParams,
     /**
      * @deprecated import directly from `vue-tg` instead.
      */
